a4468ff17ea47ba050a1818d908a9c239c20c96b
username <example@example.com>
2017-12-02 22:28:01 +0300

    task5

---
+++ task5/solve.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "net/http"
+
+func main() {
+    dict := make(map[string]string)
+    num := 0;
+    
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        path := r.URL.Path[1:]
+        if path == "" && r.Method == "POST" {
+            r.ParseForm()
+            num += 1
+            dict[string(num)] = r.Form.Get("url")
+            
+            w.Header().Set("Content-Type", "application/json")
+            fmt.Fprintf(w, "{key:%d}", num)
+        } else if path != "" {
+			url, ok := dict[path]
+			if ok {
+				http.Redirect(w, r, url, http.StatusMovedPermanently)
+			}
+        }
+    })
+    
+    http.ListenAndServe(":8080", nil)
+}
