ee8e1a4c8ad56947f54301e12b91669ae018294b
username <example@example.com>
2017-12-02 23:07:32 +0300

    task5

--- task5/solve.go
+++ task5/solve.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "net/http"
 import "strconv"
+import "strings"
 
 func main() {
     dict := make(map[string]string)
@@ -12,7 +13,7 @@ func main() {
         path := r.URL.Path[1:]
         if path == "" && r.Method == "POST" {
             num += 1
-            dict[strconv.Itoa(num)] = r.FormValue("url")
+            dict[strconv.Itoa(num)] = strings.Split(r.FormValue("body"), "\"")[3]
             
             w.Header().Set("Content-Type", "application/json")
             fmt.Fprintf(w, "{\"key\": \"%d\"}", num)
